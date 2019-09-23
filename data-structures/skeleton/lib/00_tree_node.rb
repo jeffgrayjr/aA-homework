692e3fdc4224e52dc1c8889586f5bc9ea963cd45
Jeff Gray <jeffgrayjr@gmail.com>
2019-09-22 22:54:04 -0500

    PolyTreeNode #init and #parent=

--- data-structures/skeleton/lib/00_tree_node.rb
+++ data-structures/skeleton/lib/00_tree_node.rb
@@ -1,3 +1,14 @@
 class PolyTreeNode
+    attr_reader :value, :parent
+    attr_accessor :children
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
 
+    def parent=(parent)
+        @parent = parent
+        parent.children << self
+    end
 end

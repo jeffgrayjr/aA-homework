74f6ab59f580caeec7762347072786dca9cbb24e
Jeff Gray <jeffgrayjr@gmail.com>
2019-09-23 21:02:18 -0500

    bfs is working

--- data-structures/skeleton/lib/00_tree_node.rb
+++ data-structures/skeleton/lib/00_tree_node.rb
@@ -13,7 +13,31 @@ class PolyTreeNode
         if parent != nil && !parent.children.include?(self)
             parent.children << self
             old_parent.children.delete(self) if old_parent != nil
+        end  
+    end
+
+    def add_child(child_node)
+        @children << child_node
+        child_node.parent = self
+    end
+
+    def remove_child(child_node)
+        raise "Passed node was not a child" if !@children.include?(child_node)
+        @children.delete(child_node)
+        child_node.parent = nil
+    end
+
+    def dfs(target_value)
+        true
+    end
+
+    def bfs(target_value)
+        queue = [self]
+        until queue.length == 0
+            ele = queue.shift
+            return ele if ele.value == target_value
+            ele.children.each {|child| queue << child}
         end
-        
+        nil
     end
 end

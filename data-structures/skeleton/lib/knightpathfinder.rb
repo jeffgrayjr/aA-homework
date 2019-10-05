80c448b45027490d377cf471364dcb744d6d37f4
Jeff Gray <jeffgrayjr@gmail.com>
2019-10-04 22:44:53 -0500

    added build move tree

--- data-structures/skeleton/lib/knightpathfinder.rb
+++ data-structures/skeleton/lib/knightpathfinder.rb
@@ -3,6 +3,7 @@ require_relative "00_tree_node.rb"
 class KnightPathFinder
     def initialize(starting_pos)
         @root_node = PolyTreeNode.new(starting_pos)
+        @considered_positions = []
     end
 
     def self.valid_move(pos)
@@ -18,5 +19,22 @@ class KnightPathFinder
             move[0] >= 0 && move[0] < 8 && move[1] >= 0 && move[1] < 8
         end
         return valid_moves
+    end
+
+    def new_move_positions(pos)
+        valid_moves = KnightPathFinder.valid_move(pos).select {|move| !@considered_positions.include?(move)}
+        @considered_positions += valid_moves
+        return valid_moves
+    end
+
+    def build_move_tree
+        queue = [@root_node]
+        until queue.length == 0
+            ele = queue.shift
+            children = self.new_move_positions(ele.value)
+            p children
+            p "\n"
+            children.each {|child| queue << PolyTreeNode.new(child)}
+        end
     end
 end
